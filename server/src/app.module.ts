566002dc2d0e47f6867c7ce7581e9041d225f312
JinMo-source <qkrwslah123@naver.com>
2023-05-30 00:40:49 +0900

    ConfigModules add

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,23 +1,35 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
-import { DateTimeScalar } from './scalars/datetime.scalar';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { BoardModule } from './board/board.module';
 import { Board } from './board/entities/board.entity';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    AuthModule,
-    BoardModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'parkjinmo',
-      password: 'qkrwlsah123',
-      database: 'medium',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Board],
       synchronize: true,
       logging: true,
@@ -27,6 +39,8 @@ import { AuthModule } from './auth/auth.module';
       autoSchemaFile: true,
       playground: true,
     }),
+    AuthModule,
+    BoardModule,
   ],
 })
 export class AppModule {}
